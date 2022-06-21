69bdd9a50e504212ed799b4f46b753c2bcb74e54
ivanmirandaortega <imirandaortega.pd@gmail.com>
2022-06-20 18:16:57 -0700

    used static query hook to export test data

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,17 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Recipe website",
+    author: "@ivanmirandaortega",
+    person: { name: "ivan", age: 25 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "ivan", age: 25 },
+      { name: "john", age: 25 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
