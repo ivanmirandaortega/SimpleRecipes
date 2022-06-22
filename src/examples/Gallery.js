0b631c9bb530a3dbf06edf8913e53fc54cfeec8e
ivanmirandaortega <imirandaortega.pd@gmail.com>
2022-06-21 21:53:16 -0700

    set up gallery component with graphql data

---
+++ src/examples/Gallery.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+
+const query = graphql`
+  {
+    allFile(filter: { extension: { ne: "svg" } }) {
+      nodes {
+        name
+        childrenImageSharp {
+          gatsbyImageData(
+            layout: FIXED
+            placeholder: BLURRED
+            transformOptions: { grayscale: true }
+          )
+        }
+      }
+    }
+  }
+`
+
+const Gallery = () => {
+  const data = useStaticQuery(query)
+  console.log(data)
+  return (
+    <div>
+      <h2>Simple image gallery</h2>
+    </div>
+  )
+}
+
+export default Gallery
