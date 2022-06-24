98a8ad6ff70ee2db1671bbf503b6b4728ad16862
ivanmirandaortega <imirandaortega.pd@gmail.com>
2022-06-23 21:51:58 -0700

    rendered data from tags page

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,11 +1,39 @@
 import React from "react"
 import Layout from "../components/Layout"
-const Tags = () => {
+import { Link, graphql } from "gatsby"
+import setupTags from "../utils/setupTags"
+
+const Tags = ({ data }) => {
+  const newTags = setupTags(data.allContentfulRecipe.nodes)
   return (
     <Layout>
-      <h1>tags page</h1>
+      <main className="page">
+        <section className="tags-page">
+          {newTags.map((tag, index) => {
+            const [text, value] = tag
+            return (
+              <Link to={`/${text}`} key={index} className="tag">
+                <h5>{text}</h5>
+                <p>{value}</p>
+              </Link>
+            )
+          })}
+        </section>
+      </main>
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allContentfulRecipe {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`
 
 export default Tags
