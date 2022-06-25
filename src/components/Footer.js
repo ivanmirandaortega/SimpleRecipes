b966330da30f7c188d495904d7cada1af5cd9583
ivanmirandaortega <imirandaortega.pd@gmail.com>
2022-06-24 21:32:18 -0700

    changed footer and favicon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="page-footer">
       <p>
-        &copy; {new Date().getFullYear()} <span>SimpleRecipes</span>
+        &copy; {new Date().getFullYear()} <span>SimplyRecipes</span>
       </p>
     </footer>
   )
